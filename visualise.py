689ba8fda3e31c7173b9d816f7e9e7afbffc79b1
kankean <kankean@gmail.com>
2020-06-18 11:38:07 +1000

    edited for 6FFF

--- visualise.py
+++ visualise.py
@@ -6,21 +6,27 @@ Created on Mon Jun 15 9:10:13 2020
 @author: Kaan
 """
 from bokeh.plotting import figure, output_file, show
-from bokeh.palettes import YlGn, YlGnBu
+from bokeh.palettes import YlOrRd9, Set1_9
 
 def visualise(ioned, headers):
 
     # prepare some data
     
     # output to static HTML file
-    output_file("10FFF_recombination.html")
+    output_file("6FFF_recombination.html")
+    
+    
+    TOOLTIPS = [
+    ("(depth(mm),pdd)", "($x, $y)"),
+    ]
+    
     
     # create a new plot with a title and axis labels
-    p = figure(plot_width=1200, plot_height=800, title="10FFF ion recombination corrected", x_axis_label='depth(mm)', 
-               y_axis_label='pdd')
+    p = figure(plot_width=1200, plot_height=800, title="6FFF ion recombination corrected", x_axis_label='depth(mm)', 
+               y_axis_label='pdd', tooltips=TOOLTIPS)
     
     # add a line renderer with legend and line thickness
-    for data, name, color1, color2 in zip(ioned, headers, YlGn[9], YlGnBu[9][2:]):
+    for data, name, color1, color2 in zip(ioned, headers, YlOrRd9, Set1_9):
         p.line(data[:,2], data[:,3], legend_label=name, color=color1, line_width=2)
         p.line(data[:,2], data[:,4], legend_label=name, color=color2, line_width=2)
     
@@ -28,6 +34,6 @@ def visualise(ioned, headers):
     # show the results
     p.legend.location = "top_right"
     p.legend.click_policy="hide"
-    p.toolbar.active_inspect = [hover_tool, crosshair_tool]
+    
     
     show(p)
